b25d9abf8b3618c78d4d18a39af164299ea324b5
罗国雄 <1025558554@qq.com>
2021-01-07 11:52:22 +0800

    修改

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const config = {
-  entry: "./lib/index.js",
-  output: {
-    path: path.join(__dirname, "build"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
+    entry: './lib/index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    devServer: {
+        port: 3100,
+        open: true,
+        quiet: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'front-monitor',
+            template: './webpack/template.html',
+        }),
     ],
-  },
-  devServer: {
-    port: 3100,
-    open: true,
-    quiet: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "front-monitor",
-      template: "./webpack/template.html",
-    }),
-  ],
 };
 module.exports = config;
